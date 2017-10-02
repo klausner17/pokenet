ca4dd54ccf674f8cebd8fe4881ca17dcf36f23c7
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-10-01 21:20:51 -0300

    Preparando arquivos.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const clean = require('gulp-clean');
+const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+
+// pull in the project TypeScript config
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', () => {
+  const tsResult = tsProject.src()
+  .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['scripts'], () => {
+  gulp.watch(['src/**/*.ts','!src/**/*.ts'], ['scripts'])
+});
+
+gulp.task('assets', function() {
+  return gulp.src(JSON_FILES)
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('data', function() {
+  return gulp.src('./src/**/*.json')
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['watch', 'assets']);
+gulp.task('gulp-copy', ['data']);
