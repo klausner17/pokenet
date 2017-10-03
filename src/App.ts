dd402ab776ee5cbf2aef47142369210114c60a79
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-10-02 23:40:33 -0300

    Adicionando o json de configuração do banco e alterando o app.ts para connectar no mongo.

--- src/App.ts
+++ src/App.ts
@@ -2,15 +2,19 @@ import * as path from 'path';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose'
 import ListRouter from './routes/ListRouter';
+const config = require("./app.json");
 
 class App{
+
     public express : express.Application;
 
     constructor(){
         this.express = express();
         this.middleware();
         this.routes();
+        this.mongoConnect();
     }
 
     private middleware() : void {
@@ -21,6 +25,17 @@ class App{
 
     private routes() : void {
         this.express.use('/api/listraid', ListRouter);
+    }
+
+    private mongoConnect(){
+        let db = config.connectionMongo.database;
+        let server =  config.connectionMongo.server;
+        let port = config.connectionMongo.port;
+        let conectionString : string = 'mongodb://' + server + ':' + port + '/' + db;
+        mongoose.connect(conectionString, (err) => {
+            if(err) console.log("Erro ao conectar a instancia. Erro" + err);
+            else console.log("Conectado ao banco.");
+        });
     }
 }
 
