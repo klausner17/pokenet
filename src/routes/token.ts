dee727ff6857477028c2344d2248ed7e161b684c
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-13 12:42:32 -0200

    Normalizado arquivo

--- src/routes/token.ts
+++ src/routes/token.ts
@@ -1,21 +1,21 @@
-import { Router, Request, Response } from 'express';
-import * as express from 'express';
-import { User } from '../models/User';
-import * as jwt from 'jwt-simple';
-const config = require('../config.json')['authentication'];
+import { Router, Request, Response } from "express";
+import * as express from "express";
+import { User } from "../models/User";
+import * as jwt from "jwt-simple";
+const config: any = require("../config.json").authentication;
 
 
 var tokenRouter: Router = express.Router();
 
-tokenRouter.post('/token', (req: Request, res: Response) => {
+tokenRouter.post("/token", (req: Request, res: Response) => {
     if(req.body.email && req.body.password) {
-        let email = req.body.email;
-        let pass = req.body.password;
+        let email: string  = req.body.email;
+        let pass: string = req.body.password;
         User.findOne({where: {email: email}})
             .then((user: User) => {
-                if (user.verifyPassword(pass)){
-                    let payload = {id: user.id};
-                    res.status(200).json({token: jwt.encode(payload, config['secretKey'])})
+                if (user.verifyPassword(pass)) {
+                    let payload: {} = {id: user.id};
+                    res.status(200).json({token: jwt.encode(payload, config.secretKey)});
                 } else {
                     res.sendStatus(401);
                 }
