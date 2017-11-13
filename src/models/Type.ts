c9355fb31052c1982f29784857f5004a476cd7eb
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-13 13:58:00 -0200

    Refactory do weakness

--- src/models/Type.ts
+++ src/models/Type.ts
@@ -1,14 +1,10 @@
 import { Column, HasMany, CreatedAt, UpdatedAt } from 'sequelize-typescript';
 import { Model, Table } from 'sequelize-typescript';
-import { Weakness } from './Weakness';
 
 @Table
 export class Type extends Model<Type> {
     @Column
     type: string;
-
-    @HasMany(() => Weakness)
-    weakness: Weakness[];
 
     @CreatedAt
     createdAt: Date;
