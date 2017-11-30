24bbab7cbe1bde019a7921618eb064a19594f5d7
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-30 15:46:29 -0200

    if e else do diabo

--- src/boot.ts
+++ src/boot.ts
@@ -4,7 +4,8 @@ const env = process.env.NODE_ENV;
 console.log(env);
 if (env) {
     config = require(`./config.${env}.js`);
+} else {
+    config = require("./config.development.js");
 }
-config = require("./config.development.js");
 
 export default config;
