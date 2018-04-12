93d8ab9bc69a11e2424885974b947da8bf657b20
Klausner Augusto <klausner_17@yahoo.com.br>
2018-04-12 11:18:56 -0300

    nao entendi pq esses arquivos estavam na src

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import * as express from 'express';
-const indexRoutes = express.Router();
-indexRoutes.route('/').get((req, res) => {
-    res.status(200).json({ status: 'Hello world - Pokenet API' });
-});
-export default indexRoutes;
